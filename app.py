038d412dc36eb93f5e4a19067925a709383e5b1b
Victor Jokanola <favourphilic@gmail.com>
2023-06-30 14:37:46 +0100

    new change

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.markdown('### HEllo Word! ')
 
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DT Things I love:", st.secrets["my_cool_secrets"]["things_i_like"])
+st.write("DB username:", st.secrets["DB_USERNAME"])
+
 

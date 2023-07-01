fc929f87d1ee37064610718b1ddce2f9912220ee
Victor Jokanola <favourphilic@gmail.com>
2023-07-01 03:20:51 +0100

    new commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st 
-
+import pandas as pd
 st.markdown('### HEllo Word! ')
 
-st.header('FRIDAY')
+st.header('Its a new Day')
 #st.write("DB username:", st.secrets["db_username"])
 
 
-st.sidebar.header("Welcome Home")
-st.sidebar.markdown("### Love Lives Here")
+st.sidebar.header("Home Tab")
+st.sidebar.markdown("### More details later......")
+
+
+

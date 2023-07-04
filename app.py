cdd496a05e403ffedf550738d7e788b4a9144ca0
Victor Jokanola <favourphilic@gmail.com>
2023-07-04 02:22:03 +0100

    new commit

--- app.py
+++ app.py
@@ -1,33 +1,14 @@
-import streamlit as st
-st.title('🖼️ yt-img-app')
-st.header('YouTube Thumbnail Image Extractor App')
-with st.expander('About this app'):
-    st.write('This app retrieves the thumbnail image from a YouTube video.')
-# Image settings
-st.sidebar.header('Settings')
-img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
-selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
-img_quality = img_dict[selected_img_quality]
+import streamlit as st 
+import pandas as pd
+import altair as alt
+st.markdown(''' # **Welcome**''')
 
-yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/JwSS70SZdyM')
+df= pd.read_csv('nba-2.csv')
 
-def get_ytid(input_url):
-  if 'youtu.be' in input_url:
-    ytid = input_url.split('/')[-1]
-  if 'youtube.com' in input_url:
-    ytid = input_url.split('=')[-1]
-  return ytid
-#yayi
+st.write(df.head())
 
-
-# Display YouTube thumbnail image
-if yt_url != '':
-  ytid = get_ytid(yt_url) # yt or yt_url
-
-  yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
-  st.image(yt_img)
-  st.write('YouTube video thumbnail image URL: ', yt_img)
-else:
-  st.write('☝️ Enter URL to continue ...')
-
-
+agedist=alt.Chart(df).mark_bar().encode(
+    alt.X("Age", bin=True),
+    y='count()',
+)
+st.altair_chart(agedist, use_container_width=True)
